95cc5825f6d2f7369230151239248f44952050a0
suxga <sugano.koya.b0@s.mail.nagoya-u.ac.jp>
2025-01-13 18:55:56 +0900

    Update index.tsx

--- backend/index.tsx
+++ backend/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'; // React 18以降はreact-dom/clientを使用
-import App from './App';
+import App from 'C:\\Apache24\\htdocs\\basic\\Nutrition Manager\\src\\frontend\\App.tsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(<App />);
