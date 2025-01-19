02a942a9d541b32603737ce40f227dc422b1ee04
suxga <sugano.koya.b0@s.mail.nagoya-u.ac.jp>
2025-01-19 11:31:31 +0900

    Update App.tsx

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,11 +1,10 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import FoodList from './components/FoodList'; // FoodListコンポーネントをインポート
+import NutritionTracker from './components/NutritionTracker';
 
 // Axiosの基本設定を変更
 axios.defaults.baseURL = 'http://localhost:3000';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
-axios.defaults.withCredentials = false;
 
 
 // 食品データの型を定義
@@ -15,9 +14,8 @@ type Food = {
   protein: number;
   carbs: number;
   fats: number;
+  dietaryfibers: number;
 };
-
-axios.defaults.withCredentials = false;
 
 const App: React.FC = () => {
   const [newFood, setNewFood] = useState<Food>({
@@ -26,14 +24,15 @@ const App: React.FC = () => {
     protein: 0,
     carbs: 0,
     fats: 0,
+    dietaryfibers: 0,
   });
 
   const addFood = () => {
     axios
-      .post('http://localhost:3000/api/foods', newFood)
+      .post('/api/foods', newFood)
       .then((response) => {
         console.log('Food added successfully:', response.data);
-        setNewFood({ name: '', calories: 0, protein: 0, carbs: 0, fats: 0 });
+        setNewFood({ name: '', calories: 0, protein: 0, carbs: 0, fats: 0, dietaryfibers: 0 });
       })
       .catch((error) => {
         console.error(
@@ -46,47 +45,54 @@ const App: React.FC = () => {
   return (
     <div>
       <h1>Nutrition Manager</h1>
-      <FoodList /> {/* 食品データを表示するコンポーネント */}
+      <NutritionTracker />
       <h2>食品を追加</h2>
-      <input
-        type="text"
-        placeholder="Name"
-        value={newFood.name}
-        onChange={(e) => setNewFood({ ...newFood, name: e.target.value })}
-      />
-      <input
-        type="number"
-        placeholder="Calories"
-        value={newFood.calories}
-        onChange={(e) =>
-          setNewFood({ ...newFood, calories: parseFloat(e.target.value) })
-        }
-      />
-      <input
-        type="number"
-        placeholder="Protein"
-        value={newFood.protein}
-        onChange={(e) =>
-          setNewFood({ ...newFood, protein: parseFloat(e.target.value) })
-        }
-      />
-      <input
-        type="number"
-        placeholder="Carbs"
-        value={newFood.carbs}
-        onChange={(e) =>
-          setNewFood({ ...newFood, carbs: parseFloat(e.target.value) })
-        }
-      />
-      <input
-        type="number"
-        placeholder="Fats"
-        value={newFood.fats}
-        onChange={(e) =>
-          setNewFood({ ...newFood, fats: parseFloat(e.target.value) })
-        }
-      />
-      <button onClick={addFood}>Add Food</button>
+      <div style={{ marginBottom: '20px' }}>
+        <input
+          type="text"
+          placeholder="Name"
+          value={newFood.name}
+          onChange={(e) => setNewFood({ ...newFood, name: e.target.value })}
+          style={{ marginRight: '10px' }}
+        />
+        <input
+          type="number"
+          placeholder="Calories"
+          value={newFood.calories}
+          onChange={(e) =>
+            setNewFood({ ...newFood, calories: parseFloat(e.target.value) })
+          }
+          style={{ marginRight: '10px' }}
+        />
+        <input
+          type="number"
+          placeholder="Protein"
+          value={newFood.protein}
+          onChange={(e) =>
+            setNewFood({ ...newFood, protein: parseFloat(e.target.value) })
+          }
+          style={{ marginRight: '10px' }}
+        />
+        <input
+          type="number"
+          placeholder="Carbs"
+          value={newFood.carbs}
+          onChange={(e) =>
+            setNewFood({ ...newFood, carbs: parseFloat(e.target.value) })
+          }
+          style={{ marginRight: '10px' }}
+        />
+        <input
+          type="number"
+          placeholder="Fats"
+          value={newFood.fats}
+          onChange={(e) =>
+            setNewFood({ ...newFood, fats: parseFloat(e.target.value) })
+          }
+          style={{ marginRight: '10px' }}
+        />
+        <button onClick={addFood}>Add Food</button>
+      </div>
     </div>
   );
 };
